8d8cdcfd15d488b0c89c1f20fc8f7c169cf70e28
yb00 <yagobraz98@gmail.com>
2021-04-02 22:19:16 -0300

    Add python script

---
+++ make_batchlist.py
@@ -0,0 +1,28 @@
+"""
+Makes batch list to be used with ffmpeg.
+Example usage command: youtube-dl -f "bestaudio/best" -v -x --audio-format vorbis --audio-quality 0 --batch-file download.txt -o "Tracks/Music_%(autonumber)1d.%(ext)s"
+"""
+
+import csv
+
+def main():
+    """
+    Outputs values containing desired URLs from target column in a .csv to a .txt file.
+    """
+    inputf = './tracklist.csv'
+    outputf = './batchlist.txt'
+    target = 3
+
+    txt = open(outputf, 'w+')
+    batch_list = []
+    with open(inputf) as input_file:
+        csv_file = csv.reader(input_file, delimiter=',')
+        for row in csv_file:
+            batch_list.append(row)
+        batch_list = batch_list[1:]
+
+    for row in batch_list:
+        txt.write(f'{row[target]}\n')
+
+if __name__ == '__main__':
+    main()
